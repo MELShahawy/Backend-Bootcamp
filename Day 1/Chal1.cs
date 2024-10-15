ee48cb0eba9393798dfb35777b02a9024be13040
Mohamed El Shahawy <ms_elshahawy@yahoo.com>
2024-10-15 14:38:35 +0300

    1st and 3rd quiz Day1

---
+++ Day 1/Chal1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D1Cha1
+{
+    internal class Chal1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Sign Up ");
+            Console.WriteLine("Your Name: ");
+            string name = Console.ReadLine();
+            name=name.Trim();
+            Console.WriteLine("Your Password: ");
+            string passCode = Console.ReadLine();
+            passCode=passCode.Trim();
+
+            Console.WriteLine("To Log In Please enter your name and password");
+            Console.WriteLine("Your Name: ");
+            var name1 = Console.ReadLine();
+            Console.WriteLine("Your Password: ");
+            var passCode1 = Console.ReadLine();
+
+            if (passCode == passCode1 && name == name1)
+            {
+                Console.WriteLine("Correct Log In");
+
+            }
+            else { Console.WriteLine("Wrong Name Or Passcode"); }
+
+            Console.ReadLine(); 
+        }
+}}
---
+++ Day 1/Chal3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D1Cha3
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1;
+            string input2;
+            Console.WriteLine("Enter The First Number:");
+            input1 = Console.ReadLine();
+            Console.WriteLine("Enter The First Number:");
+            input2 = Console.ReadLine();
+            int num1 = int.Parse(input1);
+            int num2 = int.Parse(input2);
+            int sum;
+            int oper;
+            Console.WriteLine(" Please Choose THe Operator From  1 for +, 2 for -, 3 for *, 4 for / ");
+            string ch = Console.ReadLine();
+            oper = int.Parse(ch);
+
+            switch (oper)
+            {
+                case 1:
+                    sum = num1 + num2;
+                    Console.WriteLine($"The Result Is {sum}");
+                    break;
+                case 2:
+                    sum = num1 - num2;
+                    Console.WriteLine($"The Result Is {sum}");
+                    break;
+                case 3:
+                    sum = num1 * num2;
+                    Console.WriteLine($"The Result Is {sum}");
+                    break;
+                case 4:
+                    sum = num1 / num2;
+                    Console.WriteLine($"The Result Is {sum}");
+                    break;
+                default:
+                    Console.WriteLine("");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
